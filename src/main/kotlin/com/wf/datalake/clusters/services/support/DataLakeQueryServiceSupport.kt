b722677b2261a460de3c77cfa085089acf0966ac
Dean Jones <deansjones@gmail.com>
2019-05-02 13:54:32 -0400

    cloud

--- src/main/kotlin/com/wf/datalake/clusters/services/support/DataLakeQueryServiceSupport.kt
+++ src/main/kotlin/com/wf/datalake/clusters/services/support/DataLakeQueryServiceSupport.kt
@@ -3,18 +3,13 @@ package com.wf.datalake.clusters.services.support
 import co.mercenary.creators.core.kotlin.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.JdbcTemplate
-import javax.sql.DataSource
 
 abstract class DataLakeQueryServiceSupport : DataLakeServiceSupport() {
 
     @Autowired
-    private lateinit var database: DataSource
-
-    private val template: JdbcTemplate by lazy {
-        JdbcTemplate(database)
-    }
+    private lateinit var template: JdbcTemplate
 
     protected fun template() = template
 
-    protected fun query(sql: String, key: String = "results") = json(key to template().queryForList(sql), "uuid" to uuid())
+    protected fun query(sql: String, key: String = "results") = json(key to template().queryForList(sql))
 }
